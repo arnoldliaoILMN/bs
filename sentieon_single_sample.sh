b9a547f4127a32298640302c32277476bdb9e876
arnoldliaoILMN <aliao@illumina.com>
2017-05-08 10:57:04 -0700

    incomplete draft for hg38
    started changes for hg38.  putting it in a brach

--- sentieon_single_sample.sh
+++ sentieon_single_sample.sh
@@ -49,6 +49,15 @@ then
     tar -I lbzip2 -xvf b37_decoy.tar.bz2
     fasta="/data/scratch/human_g1k_v37_decoy.fasta"
     ls $fasta
+elif [ "$reference" == "hg38" ]
+then
+    echo Using $reference 
+    ref_short="hg38"
+    aria2c -x 16 -s 16  https://s3.amazonaws.com/gatkres/GATK.hg38.
+#    wget https://s3.amazonaws.com/illumina-ukch-compbio/sentieon_resources/b37_decoy.tar.bz2 --quiet
+    tar -I lbzip2 -xvf b37_decoy.tar.bz2
+    fasta="/data/scratch/human_g1k_v37_decoy.fasta"
+    ls $fasta
 else
     echo unknown reference genome: $reference
     exit 1
